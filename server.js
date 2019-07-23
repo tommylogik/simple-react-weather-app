085f3e1c80cfa2711501c9d8f0e81b48f005a31b
Tommy Gonzalez <tommylogik@gmail.com>
2019-07-23 18:17:31 -0400

    simple api server to serve react build and get weather information from darksky

---
+++ server.js
@@ -0,0 +1,61 @@
+require('dotenv').config({ path: '.env.server' });
+
+const fastify = require('fastify')({ logger: true });
+const fastifyStatic = require('fastify-static');
+const fetch = require("node-fetch");
+const zipcodes = require('zipcodes');
+const path = require('path');
+
+fastify.register(fastifyStatic, {
+  root: path.join(__dirname, 'build'),
+})
+
+fastify.get('/', async (req, resp) => {
+  resp.sendFile('index.html');
+});
+
+fastify.get('/api/', async (req, resp) => {
+  if (Object.prototype.hasOwnProperty.call(req.query, 'zipcode')) {
+    // change zipcode to lat,lng
+    const geolocation = zipcodes.lookup(req.query.zipcode);
+
+
+    if (!geolocation ||
+      !Object.prototype.hasOwnProperty.call(geolocation, 'latitude') ||
+      !Object.prototype.hasOwnProperty.call(geolocation, 'longitude')) {
+      throw new Error("Zipcode seems to be invalid. Please try again.");
+    }
+    // make request to dark sky
+    const weatherUrl = `https://api.darksky.net/forecast/${process.env.DARK_SKY_API_KEY}/${geolocation.latitude},${geolocation.longitude}`;
+
+    const getData = async url => {
+      let json;
+      try {
+        const response = await fetch(url);
+        json = await response.json();
+      } catch (error) {
+        fastify.log.error(error);
+      }
+
+      return json;
+    };
+
+    const response = await getData(weatherUrl);
+    resp.code(200).send(response);
+  } else {
+    throw new Error("Route requires 'zipcode'");
+  }
+});
+
+
+const start = async () => {
+  try {
+    await fastify.listen(3001);
+    fastify.log.info(`Server listening on ${fastify.server.address().port}`)
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
